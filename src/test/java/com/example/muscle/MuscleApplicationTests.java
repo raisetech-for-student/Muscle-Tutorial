339931a92d7ff8f86dedb6d862c28ac5a46de632
yoshi_desu <xingdiansongtian@gmail.com>
2022-03-24 15:44:50 +0900

    RestTutorial

---
+++ src/test/java/com/example/muscle/MuscleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.muscle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuscleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
