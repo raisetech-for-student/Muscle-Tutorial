da48d5201c4b8eeac8c3e1c5c5777b6f88eb2360
yoshi_desu <xingdiansongtian@gmail.com>
2022-03-26 19:44:19 +0900

    自動ドキュメントツール導入

--- src/main/java/com/example/muscle/restservice/MuscleApplication.java
+++ src/main/java/com/example/muscle/restservice/MuscleApplication.java
@@ -1,9 +1,14 @@
 package com.example.muscle.restservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "muscle-openapi-sample",
+    description = "Spring Boot + springdoc-openapi のサンプルアプリ",
+    version = "v1"))
 public class MuscleApplication {
 
   public static void main(String[] args) {
